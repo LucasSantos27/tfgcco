57197eb1be7b0c61edfc77f4457f69b5e9b6a6b2
LucasSantos27 <lucas-unifei@outlook.com>
2022-05-30 19:58:16 -0300

    fix: change login response body and return token

--- lib/app/data/repositories/user_repository.dart
+++ lib/app/data/repositories/user_repository.dart
@@ -1,27 +1,19 @@
-import 'package:flutter/material.dart';
 import 'package:get/get_connect/connect.dart';
 import 'package:tfg_cco/app/data/models/models.dart';
 import 'package:tfg_cco/app/network/end_points.dart';
 
 class UserRepository extends GetConnect {
   Future<String> login(Credentials credentials) async {
-    String token = "";
-
     final response = await post(
       EndPoints.login,
       credentials.toJson(),
-      decoder: (body) {
-        token = body["token"];
-      },
     );
-
-    debugPrint(token);
 
     if (response.hasError) {
       throw Exception('Erro ao logar');
     }
 
-    return token;
+    return response.body['token'];
   }
 
   Future<String> register(User user) async {
