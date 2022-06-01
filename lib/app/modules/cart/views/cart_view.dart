c0129a3a6a5a6e63bd60afc0f9a84f1302304e5a
LucasSantos27 <lucas-unifei@outlook.com>
2022-06-01 19:40:42 -0300

    feat: add remove product on cart page

--- lib/app/modules/cart/views/cart_view.dart
+++ lib/app/modules/cart/views/cart_view.dart
@@ -19,68 +19,109 @@ class CartView extends GetView<CartController> {
         ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: List.generate(
-            controller.products.length,
-            (index) => Card(
-              elevation: 3,
-              child: ListTile(
-                  leading: Container(
-                    height: 64,
-                    width: 64,
-                    child: Image.network(
-                      '${EndPoints.srcImage}${controller.products[index].image}',
-                      fit: BoxFit.fill,
+      body: Obx(
+        () => Visibility(
+          visible: controller.products.isNotEmpty,
+          child: SingleChildScrollView(
+            child: Column(
+              children: List.generate(
+                controller.products.length,
+                (index) => Stack(
+                  children: [
+                    Card(
+                      elevation: 3,
+                      child: ListTile(
+                        leading: Container(
+                          height: 64,
+                          width: 64,
+                          child: Image.network(
+                            '${EndPoints.srcImage}${controller.products[index].image}',
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        title: Text(
+                          controller.products[index].name,
+                          style: context.textTheme.headline2,
+                        ),
+                        subtitle: Text(
+                          UtilBrasilFields.obterReal(
+                            controller.products[index].price.toDouble(),
+                          ),
+                          style: context.textTheme.bodyText2,
+                        ),
+                        trailing: CustomNumberPicker(
+                          initialValue: 1,
+                          maxValue: 1000,
+                          minValue: 0,
+                          step: 1,
+                          onValue: (value) {
+                            print(value.toString());
+                          },
+                        ),
+                      ),
                     ),
-                  ),
-                  title: Text(
-                    controller.products[index].name,
-                    style: context.textTheme.headline2,
-                  ),
-                  subtitle: Text(
-                    UtilBrasilFields.obterReal(
-                      controller.products[index].price.toDouble(),
-                    ),
-                    style: context.textTheme.bodyText2,
-                  ),
-                  trailing: CustomNumberPicker(
-                    initialValue: 1,
-                    maxValue: 1000,
-                    minValue: 0,
-                    step: 1,
-                    onValue: (value) {
-                      print(value.toString());
-                    },
-                  )),
+                    Positioned(
+                      right: 0,
+                      top: -10,
+                      child: IconButton(
+                          icon: Icon(
+                            Icons.delete,
+                            color: Colors.red,
+                            size: 20,
+                          ),
+                          onPressed: () {
+                            controller
+                                .removeProduct(controller.products[index]);
+                            controller.products.refresh();
+                          }),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          replacement: Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.symmetric(vertical: 256),
+            child: Text(
+              'Não há produtos no carrinho',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
       ),
-      bottomNavigationBar: Material(
-        elevation: 20,
-        child: Container(
-          height: 60,
-          color: context.theme.colorScheme.primary,
-          padding: EdgeInsets.all(16),
-          child: Row(
-            children: [
-              Expanded(
-                child: GestureDetector(
-                  onTap: () => print('continuar'),
-                  child: Text(
-                    'PROSSEGUIR',
+      bottomNavigationBar: Obx(
+        () => Visibility(
+          visible: controller.products.isNotEmpty,
+          child: Material(
+            elevation: 20,
+            child: Container(
+              height: 60,
+              color: context.theme.colorScheme.primary,
+              padding: EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () => print('continuar'),
+                      child: Text(
+                        'PROSSEGUIR',
+                        style: context.textTheme.headline1
+                            ?.copyWith(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    UtilBrasilFields.obterReal(controller.totalValue.value),
                     style: context.textTheme.headline1
                         ?.copyWith(color: Colors.white),
                   ),
-                ),
+                ],
               ),
-              Text(
-                UtilBrasilFields.obterReal(controller.totalValue.value),
-                style:
-                    context.textTheme.headline1?.copyWith(color: Colors.white),
-              ),
-            ],
+            ),
           ),
         ),
       ),
